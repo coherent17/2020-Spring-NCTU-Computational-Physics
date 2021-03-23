406347e14edde5d31cbca473493c9377ca60cd99
coherent17 <mnb51817@gmail.com>
2021-03-23 16:29:14 +0800

    test

---
+++ linear_algebra/DBR.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math 
+
+D_1=np.float64([[1,1]
+               ,[2.35,-2.35]])
+
+D_2=np.float64([[1,1]
+               ,[2.35,-2.35]])
+
+
+
+print(np.linalg.pinv(A))

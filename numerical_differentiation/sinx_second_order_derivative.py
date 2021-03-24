170bf19f7da2c0f21f29f61683bc83ae0df6ed01
coherent17 <mnb51817@gmail.com>
2021-03-24 16:02:27 +0800

    test

--- numerical_differentiation/sinx_second_order_derivative.py
+++ numerical_differentiation/sinx_second_order_derivative.py
@@ -13,12 +13,18 @@ y_ana=np.sin(x)
 dy_ana=np.cos(x)
 ddy_ana=-np.sin(x)
 
-ddy_2=[]
+ddy_3=[]
 for i in range(1,N-1):
-    ddy_2.append((y_ana[i+1]-2*y_ana[i]+y_ana[i-1])/h/h)
+    ddy_3.append((y_ana[i+1]-2*y_ana[i]+y_ana[i-1])/h/h)
 
-x2=np.delete(x,[0,N-1])
+x3=np.delete(x,[0,N-1])
 
 plt.plot(x,ddy_ana)
-plt.plot(x2,ddy_2,'r.',label="two points")
+plt.plot(x3,ddy_3,'r.',label="three points")
+plt.show()
+
+error3=(abs(ddy_3-ddy_ana[1:N-1]))
+plt.plot(x3,error3,label="three points")
+plt.title("error by using different number of points")
+plt.legend()
 plt.show()

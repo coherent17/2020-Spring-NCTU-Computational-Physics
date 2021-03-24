7fdd477b057f363dddc704cec6a211bbb00350e7
coherent17 <mnb51817@gmail.com>
2021-03-24 15:52:30 +0800

    test

--- numerical_differentiation/fluid_resistance.py
+++ numerical_differentiation/fluid_resistance.py
@@ -3,10 +3,22 @@ import matplotlib.pyplot as plt
 
 N=51
 x=np.linspace(0,5,N)
-y_ana=1-np.exp(-x)
+h=5/(N-1)
+vy=1-np.exp(-x)
 
-plt.plot(x,y_ana,label="v")
+plt.plot(x,vy,label="$v_y/v_t$")
 plt.title("Falling object in a fluid")
 plt.legend()
 plt.show()
 
+dvy_ana=1-vy
+dvy_2=[]
+for i in range(0,N-1):
+    dvy_2.append((vy[i+1]-vy[i])/h)
+x2=np.delete(x,[N-1])
+
+
+plt.plot(x,dvy_ana,label="$(1-v_y)/t_s$")
+plt.plot(x2,dvy_2,'r.',label="two points")
+plt.legend()
+plt.show()

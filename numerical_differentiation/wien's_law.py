f7201e06712ae4f671836ef37f956ec698c627a5
coherent17 <mnb51817@gmail.com>
2021-03-25 10:19:58 +0800

    test

---
+++ numerical_differentiation/wien's_law.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N=1001
+initial=1
+final=2000
+x=np.linspace(initial,final,N)
+
+
+
+def Wien(T):
+    y=[]
+    for i in x:
+        k=1.380649*(10**-23)
+        hc=1.98644586*(10**-25)
+        y.append(8*np.pi*hc/((i*(10**-9))**5)/(np.exp(hc/(i*(10**-9))/k/T)-1))
+    return y
+
+y_3500=Wien(3500)
+y_4000=Wien(4000)
+y_4500=Wien(4500)
+y_5000=Wien(5000)
+y_5500=Wien(5500)
+
+plt.plot(x,y_3500,label="$3500K$")
+plt.plot(x,y_4000,label="$4000K$")
+plt.plot(x,y_4500,label="$4500K$")
+plt.plot(x,y_5000,label="$5000K$")
+plt.plot(x,y_5500,label="$5500K$")
+plt.title("Wien's law")
+plt.xlabel("wavelength(λ)  (nm)")
+plt.ylabel("Spectral energy density")
+plt.legend()
+plt.show()

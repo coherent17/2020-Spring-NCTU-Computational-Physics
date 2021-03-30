7e68ad506d2d92776e49edfb43cd29ea71df4314
coherent17 <mnb51817@gmail.com>
2021-03-30 23:38:23 +0800

    test

--- numerical_differentiation/newton_method.py
+++ numerical_differentiation/newton_method.py
@@ -1,5 +1,33 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+N=1001
+initial=-6
+final=6
+h=(final-initial)/(N-1)
+x=np.linspace(initial,final,1001)
+y=(x**2)-3*x-10
+
+#first order differential
+dy_2=[]
+for i in range(0,N-1):
+    dy_2.append((y[i+1]-y[i])/h)
+x2=np.delete(x,[N-1])
+
 def f(x):
     return (x**2)-3*x-10
+
+def newton(x_0,eps):
+    iteration=0
+    while 1:
+        if abs(f(x_0))>eps:
+            x_0=x_0-f(x_0)/dy_2[int((x_0-initial)/h)]
+            print(x_0)
+            iteration+=1
+        else:
+            break
+    return x_0,iteration
+
+root,iteration=newton(4,0.0000001)
+print(root)
+print(iteration)
--- numerical_differentiation/wien's_law.py
+++ numerical_differentiation/wien's_law.py
@@ -7,8 +7,6 @@ final=2000
 x=np.linspace(initial,final,N)
 h=(final-initial)/(N-1)
 
-
-
 def Wien(T):
     y=[]
     for i in x:
@@ -45,25 +43,28 @@ def get_max_lamda(y_T):
 
     return y_max
 
-
+#obtain the intensity in different temperature
 y_3500=Wien(3500)
 y_4000=Wien(4000)
 y_4500=Wien(4500)
 y_5000=Wien(5000)
 y_5500=Wien(5500)
 
+#get the max lambda at different temperature unit:(nm)
 l_3500=get_max_lamda(y_3500)
 l_4000=get_max_lamda(y_4000)
 l_4500=get_max_lamda(y_4500)
 l_5000=get_max_lamda(y_5000)
 l_5500=get_max_lamda(y_5500)
 
-print(l_3500[0]*3500)
-print(l_4000[0]*4000)
-print(l_4500[0]*4500)
-print(l_5000[0]*5000)
-print(l_5500[0]*5500)
-
+#get the constant of the wein's law unit:(mm*K)
+print("At 3500K,\tλ_max=%.5f(nm)"%(l_3500[0]),"\t","λ_max*T=%.6f(mm*K)"%(l_3500[0]*3500*(10**-6)))
+print("At 4000K,\tλ_max=%.5f(nm)"%(l_4000[0]),"\t","λ_max*T=%.6f(mm*K)"%(l_4000[0]*4000*(10**-6)))
+print("At 4500K,\tλ_max=%.5f(nm)"%(l_4500[0]),"\t","λ_max*T=%.6f(mm*K)"%(l_4500[0]*4500*(10**-6)))
+print("At 5000K,\tλ_max=%.5f(nm)"%(l_5000[0]),"\t","λ_max*T=%.6f(mm*K)"%(l_5000[0]*5000*(10**-6)))
+print("At 5500K,\tλ_max=%.5f(nm)"%(l_5500[0]),"\t","λ_max*T=%.6f(mm*K)"%(l_5500[0]*5500*(10**-6)))
+λmax_avg=np.mean([l_3500[0]*3500*(10**-6),l_4000[0]*4000*(10**-6),l_4500[0]*4500*(10**-6),l_5000[0]*5000*(10**-6),l_5500[0]*5500*(10**-6)])
+print("the average of the Wien's distribution law is=%.5f(mm*K)"%(λmax_avg))
 
 plt.plot(x,y_3500,label="$3500K$")
 plt.plot(x,y_4000,label="$4000K$")
@@ -71,7 +72,8 @@ plt.plot(x,y_4500,label="$4500K$")
 plt.plot(x,y_5000,label="$5000K$")
 plt.plot(x,y_5500,label="$5500K$")
 plt.title("Wien's law")
-plt.xlabel("wavelength(λ)  (nm)")
+plt.xlabel("wavelength(λ) (nm)")
 plt.ylabel("Spectral energy density ($kJ/m^4$)")
+plt.text(1250,400,"$λ_{max}*T=%.3lf(mm*K)$"%(λmax_avg))
 plt.legend()
 plt.show()

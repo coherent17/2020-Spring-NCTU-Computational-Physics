3b28b87f6c1f9f2252ad32f22c2a92aef563ee73
coherent17 <mnb51817@gmail.com>
2021-04-23 19:44:05 +0800

    test

---
+++ partial_differential_equation/poisson's_equation_inversion_matrix_method.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def coefficient_matrix(N):
+    A=np.zeros((N,N))
+    for i in range(0,N):
+        if i==0:
+            A[i,0]=1
+        elif i==N-1:
+            A[i,-1]=1
+        else:
+            A[i,i]=-2
+            A[i,i+1]=1
+            A[i,i-1]=1
+    return A
+
+
+def rho(N):
+    rho=np.zeros((N))
+    rho[int(N/2)]=1
+    return rho
+
+
+#parameters:
+#grid number in x direction
+N=11
+L=1
+h=L/(N-1)
+x=h*np.arange(0,N)
+
+
+A=coefficient_matrix(N)
+rho=rho(N)
+V=-h*np.linalg.inv(A)@rho
+
+plt.plot(x,V)
+plt.xlabel('x(L)')
+plt.ylabel('V ($\sigma/\epsilon_0 \epsilon_b$)')
+plt.legend()
+plt.show()
--- partial_differential_equation/poisson_eq.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-def coefficient_matrix(N):
-    A=np.zeros((N,N))
-return A
-

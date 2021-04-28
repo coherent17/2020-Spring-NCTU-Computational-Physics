3c1567903ac2adf42080d01f396232609128a2ed
coherent17 <mnb51817@gmail.com>
2021-04-28 14:26:12 +0800

    test

--- partial_differential_equation/finite_parallel_plate_capacitor.py
+++ partial_differential_equation/finite_parallel_plate_capacitor.py
@@ -10,35 +10,43 @@ xx,yy=np.meshgrid(x,y)
 #BC
 V=np.zeros((11,11),dtype=float)
 
+#set the plate capacitor voltage as constant
+for i in range(1,N-1):
+    for j in range(1,N-1):
+        if j==3 and i>=3 and i<=7:
+            V[j,i]=1
+        elif j==7 and i>=3 and i<=7:
+            V[j,i]=-1
+
+V_old=V
+#update the voltage by relaxation method
 iteration=0
-while(iteration<10):
-    V_old=V
+while(True):
+    V_new=V_old
     for i in range(1,N-1):
         for j in range(1,N-1):
             #setting the parallel plate capacitor as the constant
             if j==3 and i>=3 and i<=7:
-                V[j,i]=-1
+                continue
             elif j==7 and i>=3 and i<=7:
-                V[j,i]=1
+                continue
             #relaxation method to solve the pde
             else:
-                V[j,i]=0.25*(V[j,i-1]+V[j-1,i]+V[j+1,i]+V[j,i+1])
+                V_new[j,i]=0.25*(V_old[j,i-1]+V_old[j-1,i]+V_old[j+1,i]+V_old[j,i+1])
     iteration+=1
-    V_new=V
-    print(iteration)
+    #setting the condition to break the while loop
     # #check the error before and after the iteration
-    # if iteration%1==0:
-    #     error=np.zeros((N,N),dtype=float)
-    #     for i in range(0,N):
-    #         for j in range(0,N):
-    #             error[j,i]=abs(V_new[j,i]-V_old[j,i])
-    #     print(error)
-    #     if error.all()<10**-50:
-    #         break
-    
+    if iteration%5==0: #per 5 loops to check the error
+        error=np.zeros((N,N),dtype=float)
+        error=abs(V_new-V_old)
+        print(error)
+        if error.all()<=0:
+            print(iteration)
+            break
+        else:
+            V_old=V_new
 
-        
-            
+#visualize
 fig=plt.figure(figsize=(18,6))
 ax=fig.add_subplot(121)
 z=V
@@ -49,5 +57,5 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 
 ax1=fig.add_subplot(122)
-ax1.plot(x,V[:,2])
+ax1.plot(x,V[:,3])
 plt.show()   

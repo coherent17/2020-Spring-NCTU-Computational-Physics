49286924e43a7f48fbe546fcf82c6715b8b76280
coherent17 <78169365+coherent17@users.noreply.github.com>
2021-05-04 16:16:04 +0800

    test

--- matrix_diagonalization/diagonalize_matrix_exercise.py
+++ matrix_diagonalization/diagonalize_matrix_exercise.py
@@ -15,3 +15,17 @@ print(eigenvector)
 print(a@eigenvector@np.linalg.pinv(eigenvector))
 # [[0.7 0.2]
 #  [0.3 0.8]]
+
+b=np.array([[0,-1j]
+           ,[1j,0]])
+
+eigenvalue,eigenvector=np.linalg.eig(b)
+print(eigenvalue) 
+print(eigenvector)
+# [ 1.+0.j -1.+0.j]
+# [[-0.        -0.70710678j  0.70710678+0.j        ]
+#  [ 0.70710678+0.j          0.        -0.70710678j]]
+
+print(b@eigenvector@np.linalg.pinv(eigenvector))
+# [[-3.33066907e-16-3.92523115e-17j -3.92523115e-17-1.00000000e+00j]
+#  [-3.92523115e-17+1.00000000e+00j -2.22044605e-16+3.92523115e-17j]]
